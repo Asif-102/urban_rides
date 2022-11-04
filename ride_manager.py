402342d61472aa617d95138fd5831dedb585846f
Asif-102 <asif1zero2@gmail.com>
2022-11-05 05:09:28 +0600

    find match for a ride

--- ride_manager.py
+++ ride_manager.py
@@ -16,7 +16,15 @@ class RideManager:
     def get_available_cars(self):
         return self.__available_cars
 
-    def find_a_vehicle(self):
-        pass
+    def find_a_vehicle(self, rider, vehicle_type, destination):
+        if vehicle_type == 'car':
+            if len(self.__available_cars) == 0:
+                print('sorry no cars is available')
+                return False
+            for car in self.__available_cars:
+                print('potential', rider.location, car.driver.location)
+                if abs(rider.location - car.driver.location) < 30:
+                    print('find a match for you')
+                
 
 uber = RideManager()
